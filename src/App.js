936ad4717d818d2bbab94b66072abfa9ab7a57c2
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-16 13:45:19 -0400

    Built JSX route structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./components/pages/About";
+import Discover from "./components/pages/Discover";
+import Search from "./components/pages/Search";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => (
+  <Router>
+    <div>
+      <NavTabs />
+      <Route exact path="/" component={About} />
+      <Route exact path="/discover" component={Discover} />
+      <Route exact path="/search" component={Search} />
+    </div>
+  </Router>
+);
 
 export default App;
